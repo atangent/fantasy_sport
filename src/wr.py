f6e8f07e2b4b63b9caf95a83d6acfdbcc630c9fe
atangent <amy.tang@queensu.ca>
2020-01-22 15:15:27 -0500

    update index.html

--- src/wr.py
+++ src/wr.py
@@ -3,23 +3,23 @@ import numpy as np
 from numpy import genfromtxt
 
 # Import csv files
-csv2013 = pd.read_csv("/Users/amytang/OneDrive - Queen\'s University/QMIND/data/Receiving "
+csv2013 = pd.read_csv("../data/Receiving "
                        "Stats/2013_Receiving_stats.csv",
                       keep_default_na=False, na_values=[""])
 
-csv2014 = pd.read_csv("/Users/amytang/OneDrive - Queen\'s University/QMIND/data/Receiving "
+csv2014 = pd.read_csv("../data/Receiving "
                        "Stats/2014_Receiving_stats.csv")
 
-csv2015 = pd.read_csv("/Users/amytang/OneDrive - Queen\'s University/QMIND/data/Receiving "
+csv2015 = pd.read_csv("../data/Receiving "
                        "Stats/2015_Receiving_stats.csv")
 
-csv2016 = pd.read_csv("/Users/amytang/OneDrive - Queen\'s University/QMIND/data/Receiving "
+csv2016 = pd.read_csv("../data/Receiving "
                        "Stats/2016_Receiving_stats.csv")
 
-csv2017 = pd.read_csv("/Users/amytang/OneDrive - Queen\'s University/QMIND/data/Receiving "
+csv2017 = pd.read_csv("../data/Receiving "
                        "Stats/2017_Receiving_stats.csv")
 
-csv2018 = pd.read_csv("/Users/amytang/OneDrive - Queen\'s University/QMIND/data/Receiving "
+csv2018 = pd.read_csv("../data/Receiving "
                        "Stats/2018_Receiving_stats.csv")
 
 # Concatenate arrays side by side

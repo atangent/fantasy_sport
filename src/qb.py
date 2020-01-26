638487f96994bdcdb3cf8bf06c0e291c1020a839
Adam Farley <adamfrly@gmail.com>
2020-01-26 15:18:18 -0500

    QB data frames

--- src/qb.py
+++ src/qb.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import numpy as np
 
-csv2013 = pd.read_csv("../data/QB Stats/2013_QBPassing_stats.csv")
-csv2014 = pd.read_csv("../data/QB Stats/2014_QBPassing_stats.csv")
-csv2015 = pd.read_csv("../data/QB Stats/2015_QBPassing_stats.csv")
-csv2016 = pd.read_csv("../data/QB Stats/2016_QBPassing_stats.csv")
-csv2017 = pd.read_csv("../data/QB Stats/2017_QBPassing_stats.csv")
-csv2018 = pd.read_csv("../data/QB Stats/2018_QBPassing_stats.csv")
+qb_2013 = pd.read_csv("../data/QB Stats/2013_QBPassing_stats.csv")
+qb_2014 = pd.read_csv("../data/QB Stats/2014_QBPassing_stats.csv")
+qb_2015 = pd.read_csv("../data/QB Stats/2015_QBPassing_stats.csv")
+qb_2016 = pd.read_csv("../data/QB Stats/2016_QBPassing_stats.csv")
+qb_2017 = pd.read_csv("../data/QB Stats/2017_QBPassing_stats.csv")
+qb_2018 = pd.read_csv("../data/QB Stats/2018_QBPassing_stats.csv")
 
 # Join dataframes based on unique player id
-df_2013_2014 = pd.merge(csv2013, csv2014, how="left", on=["Player", "Player"])
-df_2014_2015 = pd.merge(csv2014, csv2015, how="left", on=["Player", "Player"])
-df_2015_2016 = pd.merge(csv2015, csv2016, how="left", on=["Player", "Player"])
-df_2016_2017 = pd.merge(csv2016, csv2017, how="left", on=["Player", "Player"])
-df_2017_2018 = pd.merge(csv2017, csv2018, how="left", on=["Player", "Player"])
+qb_2013_2014 = pd.merge(qb_2013, qb_2014, how="left", on=["Player", "Player"])
+qb_2014_2015 = pd.merge(qb_2014, qb_2015, how="left", on=["Player", "Player"])
+qb_2015_2016 = pd.merge(qb_2015, qb_2016, how="left", on=["Player", "Player"])
+qb_2016_2017 = pd.merge(qb_2016, qb_2017, how="left", on=["Player", "Player"])
+qb_2017_2018 = pd.merge(qb_2017, qb_2018, how="left", on=["Player", "Player"])
 
 # Concatenate all frames below one another
-dfs = [df_2013_2014, df_2014_2015, df_2015_2016, df_2016_2017, df_2017_2018]
-entire_df = pd.concat(dfs)
+qb_dfs = [qb_2013_2014, qb_2014_2015, qb_2015_2016, qb_2016_2017, qb_2017_2018]
+qb_df = pd.concat(dfs)
 
 

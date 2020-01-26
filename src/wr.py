cccee0647734cc8b4fee38790aaf1dd869284159
atangent <amy.tang@queensu.ca>
2020-01-26 12:18:35 -0500

    merged into one huge DF :)

--- src/wr.py
+++ src/wr.py
@@ -22,7 +22,13 @@ csv2017 = pd.read_csv("../data/Receiving "
 csv2018 = pd.read_csv("../data/Receiving "
                        "Stats/2018_Receiving_stats.csv")
 
-# Concatenate arrays side by side
-df1314 = pd.concat([csv2013, csv2014], axis=1)
-
 # Join dataframes based on unique player id
+df_2013_2014 = pd.merge(csv2013, csv2014, how="left", on=["Player", "Player"])
+df_2014_2015 = pd.merge(csv2014, csv2015, how="left", on=["Player", "Player"])
+df_2015_2016 = pd.merge(csv2015, csv2016, how="left", on=["Player", "Player"])
+df_2016_2017 = pd.merge(csv2016, csv2017, how="left", on=["Player", "Player"])
+df_2017_2018 = pd.merge(csv2017, csv2018, how="left", on=["Player", "Player"])
+
+# Concatenate all frames below one another
+dfs = [df_2013_2014, df_2014_2015, df_2015_2016, df_2016_2017, df_2017_2018]
+entire_df = pd.concat(dfs)

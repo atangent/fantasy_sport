e6c146f44e686b09f0bb76e57041727be8adfff9
Adam Farley <adamfrly@gmail.com>
2020-01-26 15:12:24 -0500

    QB data frames

--- src/qb.py
+++ src/qb.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import numpy as np
 
-data2013 = pd.read_csv("../data/QB Stats/2013_QBPassing_stats.csv")
-
-data2014 = pd.read_csv("..data/QB Stats/2014_QBPassing_stats.csv")
+csv2013 = pd.read_csv("../data/QB Stats/2013_QBPassing_stats.csv")
+csv2014 = pd.read_csv("../data/QB Stats/2014_QBPassing_stats.csv")
+csv2015 = pd.read_csv("../data/QB Stats/2015_QBPassing_stats.csv")
+csv2016 = pd.read_csv("../data/QB Stats/2016_QBPassing_stats.csv")
+csv2017 = pd.read_csv("../data/QB Stats/2017_QBPassing_stats.csv")
+csv2018 = pd.read_csv("../data/QB Stats/2018_QBPassing_stats.csv")
 
 # Join dataframes based on unique player id
 df_2013_2014 = pd.merge(csv2013, csv2014, how="left", on=["Player", "Player"])

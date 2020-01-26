53544130b8384bc5985caeb395e7c66edc5533f0
atangent <amy.tang@queensu.ca>
2020-01-26 15:10:08 -0500

    runnerback df

--- src/rb.py
+++ src/rb.py
@@ -21,11 +21,11 @@ rb_2018 = pd.read_csv("../data/Rushing "
 
 # Join dataframes based on unique player id
 rb_2013_2014 = pd.merge(rb_2013, rb_2014, how="left", on=["Player", "Player"])
-df_2014_2015 = pd.merge(csv2014, csv2015, how="left", on=["Player", "Player"])
-df_2015_2016 = pd.merge(csv2015, csv2016, how="left", on=["Player", "Player"])
-df_2016_2017 = pd.merge(csv2016, csv2017, how="left", on=["Player", "Player"])
-df_2017_2018 = pd.merge(csv2017, csv2018, how="left", on=["Player", "Player"])
+rb_2014_2015 = pd.merge(rb_2014, rb_2015, how="left", on=["Player", "Player"])
+rb_2015_2016 = pd.merge(rb_2015, rb_2016, how="left", on=["Player", "Player"])
+rb_2016_2017 = pd.merge(rb_2016, rb_2017, how="left", on=["Player", "Player"])
+rb_2017_2018 = pd.merge(rb_2017, rb_2018, how="left", on=["Player", "Player"])
 
 # Concatenate all frames below one another
-dfs = [df_2013_2014, df_2014_2015, df_2015_2016, df_2016_2017, df_2017_2018]
-rb_df = pd.concat(dfs)
+rb_dfs = [rb_2013_2014, rb_2014_2015, rb_2015_2016, rb_2016_2017, rb_2017_2018]
+rb_df = pd.concat(rb_dfs)

b026eaf63f8309a1cc47f5adf0630c183528291f
atangent <amy.tang@queensu.ca>
2020-01-26 16:26:59 -0500

    tried to output as csv but gtg no time

--- src/rb_df.py
+++ src/rb_df.py
@@ -1,23 +1,17 @@
 import pandas as pd
 
 # Import csv files
-rb_2013 = pd.read_csv("../data/Rushing "
-                       "Stats/2013_Rushing_stats.csv")
+rb_2013 = pd.read_csv("../data/Rushing Stats/2013_Rushing_stats.csv")
 
-rb_2014 = pd.read_csv("../data/Rushing "
-                       "Stats/2014_Rushing_stats.csv")
+rb_2014 = pd.read_csv("../data/Rushing Stats/2014_Rushing_stats.csv")
 
-rb_2015 = pd.read_csv("../data/Rushing "
-                       "Stats/2015_Rushing_stats.csv")
+rb_2015 = pd.read_csv("../data/Rushing Stats/2015_Rushing_stats.csv")
 
-rb_2016 = pd.read_csv("../data/Rushing "
-                       "Stats/2016_Rushing_stats.csv")
+rb_2016 = pd.read_csv("../data/Rushing Stats/2016_Rushing_stats.csv")
 
-rb_2017 = pd.read_csv("../data/Rushing "
-                       "Stats/2017_Rushing_stats.csv")
+rb_2017 = pd.read_csv("../data/Rushing Stats/2017_Rushing_stats.csv")
 
-rb_2018 = pd.read_csv("../data/Rushing "
-                       "Stats/2018_Rushing_stats.csv")
+rb_2018 = pd.read_csv("../data/Rushing Stats/2018_Rushing_stats.csv")
 
 # Join dataframes based on unique player id
 rb_2013_2014 = pd.merge(rb_2013, rb_2014, how="left", on=["Player", "Player"])
@@ -29,3 +23,4 @@ rb_2017_2018 = pd.merge(rb_2017, rb_2018, how="left", on=["Player", "Player"])
 # Concatenate all frames below one another
 rb_dfs = [rb_2013_2014, rb_2014_2015, rb_2015_2016, rb_2016_2017, rb_2017_2018]
 rb_df = pd.concat(rb_dfs)
+rb_df_csv = rb_df.to_csv('../data/Dataframes/rb_df.csv')

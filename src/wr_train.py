cc82c1d845829bce70038e10948aeae005d784bb
atangent <amy.tang@queensu.ca>
2020-01-29 14:39:25 -0500

    train model

--- src/wr_train.py
+++ src/wr_train.py
@@ -3,3 +3,20 @@ from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+predict = "G3"
+X = np.array(data.drop([predict], 1))
+y = np.array(data[predict])
+best = 0
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
+    linear = linear_model.LinearRegression()
+    linear.fit(x_train, y_train)
+    acc = linear.score(x_test, y_test)
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle", "wb") as f:
+            pickle.dump(linear, f)
+
+print(best)

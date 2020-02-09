20f888477c6ba637caf4305feac382c8336c3f11
atangent <amy.tang@queensu.ca>
2020-02-09 15:14:06 -0500

    revert

--- src/wr_train.py
+++ src/wr_train.py
@@ -1,12 +1,7 @@
-#Import Libraries
-import numpy as np
 import pandas as pd
-from sklearn import linear_model
-import sklearn
-from sklearn.utils import shuffle
-import matplotlib.pyplot as plt
-from matplotlib import style
-import pickle
+from sklearn import datasets, linear_model
+from sklearn.model_selection import train_test_split
+from matplotlib import pyplot as plt
 """
 HOW TO CALCULATE FANTASY POINTS
 passing yards - 1 point /25 yards
@@ -21,50 +16,21 @@ receiving touchdowns - 6 points
 fumbles lost - -2 points
 fumble recovered before touchdown - 6 points
 """
-# Link to csv
-data = pd.read_csv("../data/Dataframes/wr_df.csv", sep=";")
-# Trim data with only inputs needed
-data = data[["Age_x", "G_x", "Gs_x", "Tgt_x", "Rec_x", "Ctch%_x", "Yds_x", "Y/R_x", "TD_x", "1D_x",
-             "Lng_x", "Y/Tgt_x", "R/G_x", "Y/G_x", "Fmb_x", "FanPt_x", "FanPosRank_x", "FanOvRank_x"]]
-# Separate data
-predict = "FanPt_y"
-# Features
-x = np.array(data[drop([predict], 1)])
-# Label
+# Code to train model for best from article 
+data = pd.read_csv("student-mat.csv", sep=";")
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences"]]
+predict = "G3"
+X = np.array(data.drop([predict], 1))
 y = np.array(data[predict])
-# Split into training testing
-x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.2)
-
-# Fun part. Train multiple models. Linear regression on steroids
 best = 0
-for _ in range(50):
-    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size = 0.2)
+for _ in range(30):
+    x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(X, y, test_size=0.1)
     linear = linear_model.LinearRegression()
     linear.fit(x_train, y_train)
-    accuracy = linear.score(x_test, y_test)
-    print("Accuracy of model: " + str(accuracy))
-    # If current model has better score than max, save as max
-    if accuracy > best:
-        best = accuracy
-        with open("../data/pickle/wr.pickle", "wb") as f:
+    acc = linear.score(x_test, y_test)
+    if acc > best:
+        best = acc
+        with open("studentmodel.pickle", "wb") as f:
             pickle.dump(linear, f)
 
-# Load model
-pickle_in = open("../data/pickle/wr.pickle", "rb")
-linear = pickle.load(pickle_in)
-
-print("Coefficient: ", linear.coef_)
-print("Intercept: ", linear.intercept_)
-
-predictions = linear.predict(x_test)
-for x in range(len(predictions)):
-    print(predictions[x], x_test[x], y_test[x])
-
-# Plot model
-plot = "failures"
-plt.scatter(data[plot], data["G3"])
-plt.legend(loc=4)
-plt.xlabel(plot)
-plt.ylabel("Fantasy points")
-plt.show()
-
+print(best)

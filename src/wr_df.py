d647b5770326972f0952d22650b68ba3f8801fec
atangent <amy.tang@queensu.ca>
2020-02-09 15:10:52 -0500

    revert

--- src/wr_df.py
+++ src/wr_df.py
@@ -1,6 +1,8 @@
 import pandas as pd
 
 # Import regular stats
+from numpy import sort
+
 wr_2013 = pd.read_csv("../data/Receiving Stats/2013_Receiving_stats.csv")
 wr_2014 = pd.read_csv("../data/Receiving Stats/2014_Receiving_stats.csv")
 wr_2015 = pd.read_csv("../data/Receiving Stats/2015_Receiving_stats.csv")
@@ -33,7 +35,7 @@ wr_2017_2018 = pd.merge(wr_all_2017, wr_all_2018, how="left", on=["Player", "Pla
 
 # Concatenate all frames below one another
 wr_dfs = [wr_2013_2014, wr_2014_2015, wr_2015_2016, wr_2016_2017, wr_2017_2018]
-wr_df = pd.concat(wr_dfs)
+wr_df = pd.concat(wr_dfs, sort-True)
 
 # Fill empty vals with 0
 wr_df = wr_df.fillna(0)
